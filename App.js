5d1a3760fa8d4767558e1952647328d066246955
Fernando Leite <fernando.leite@wlcsolucoes.com.br>
2022-01-21 17:54:11 -0300

    .

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Stack from './navigation/Stack';
+import { LogBox } from 'react-native';
 
+LogBox.ignoreLogs(['Setting a timer']);
 
 const App = () => {
   return (
